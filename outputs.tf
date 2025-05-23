8f4411ed5e9857f45bf94c44f33423064cf6dfd8
Jaime Andres Velez <jandres.vr@hotmail.com>
2025-05-23 02:33:50 -0500

    Revert "Primer commit: infraestructura Terraform EC2"
    This reverts commit d3a7597cb69dd36d359777df04a18d6dacc6c1f4.

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_ids" {
-  description = "IDs de las instancias EC2 creadas"
-  value       = module.ec2.instance_ids
-}
-
-output "public_ips" {
-  description = "IPs públicas de las instancias EC2"
-  value       = module.ec2.public_ips
-}
